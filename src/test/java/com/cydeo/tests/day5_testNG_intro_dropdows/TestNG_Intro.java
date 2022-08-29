29a3c70603de142431b66a68bf069c694dcace59
masitesahin90 <masitesahin90@gmail.com>
2022-08-29 22:39:07 +0100

    hello selenium

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdows/TestNG_Intro.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day5_testNG_intro_dropdows;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+
+    @Test
+    public void test1(){
+        System.out.println("test 1 is running");
+}
+
+}
